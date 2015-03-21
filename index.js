eb5c1ae9f377a9793475198045d8c1745a4be0e5
Rishabh Chhabra <rishabh.chhabra561@gmail.com>
2015-03-21 15:32:46 +0530

    Adds bodyParser and routes for create and update

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.engine('handlebars',handlebars.engine);
 
+app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
-app.use(express.static('/public'));
-
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+	
 app.get('/',function(req,res){
 	res.render('home');
+});
+
+app.post('/create',function(req,res){
+	var filename = 'schema/'+req.body.filename+'.json';
+	fs.exists(filename,function(exists){
+		if(!exists) {
+			fs.appendFile(filename,JSON.stringify({'val':40}));
+			res.end("File " + req.body.filename+".json created.");
+		}
+		else {
+			res.end("File " +req.body.filename+".json already exists.");
+		}
+	});
+});
+
+app.post('/update/:file/:val',function(req,res){
+	var filename = 'schema/'+res.body.file+'.json';
+	console.log(filename);
+	fs.exists(filename,function(exists){
+		if(exists) {
+			var file = require(filename);
+			file.val = req.val;
+			fs.writeFile(filename, JSON.stringify(file), function (err) {
+			  if (err) return console.log(err)
+			  console.log(JSON.stringify(file));
+			  console.log('writing to ' + filename);
+			});
+			res.send('updated');
+		}
+		else {
+			res.send("File " + res.file + " does not exist.")
+		}
+	});
 });
 
 app.use(function(req,res,next){
@@ -22,4 +58,3 @@ app.use(function(req,res,next){
 app.listen(app.get('port'),function(){
 	console.log("Listening");
 });
-
