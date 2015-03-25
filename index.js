cc017ff78931212e11368595948291a66c06e6cc
Rishabh Chhabra <rishabh.chhabra561@gmail.com>
2015-03-25 17:38:33 +0530

    Verifies token on each request

--- index.js
+++ index.js
@@ -23,22 +23,29 @@ app.get('/',function(req,res){
 	res.render('home');
 });
 
-app.get('/read/:file',function(req,res){
-	var filename = './schema/'+req.params.file+'.json';
-	fs.exists(filename,function(exists){
-		if(exists) {
-			fs.readFile(filename,'utf-8',function(err,data){
-				if (err) res.send(err);
-					res.send(data);
-			});
-		}
-		else {
-			res.send('The requested file does not exist');
-		}
-	});
+app.get('/read/:token/:file',function(req,res){
+	var token = req.params.token;
+	var verified = verifyToken(token);
+	if(verified.err) {
+		res.send("Wrong token!");
+	}
+	else {
+		var filename = './schema/'+verified.name+'/'+req.params.file+'.json';
+		fs.exists(filename,function(exists){
+			if(exists) {
+				fs.readFile(filename,'utf-8',function(err,data){
+					if (err) res.send(err);
+						res.send(data);
+				});
+			}
+			else {
+				res.send('The requested file does not exist');
+			}
+		});
+	}
 });
 
-app.get('/read/:file/:query',function(req,res){
+app.get('/read/:token/:file/:query',function(req,res){
 	var filename = './schema/'+req.params.file+'.json';
 	fs.exists(filename,function(exists){
 		if(exists) {
@@ -55,57 +62,79 @@ app.get('/read/:file/:query',function(req,res){
 
 
 app.post('/create',function(req,res){
-	var filename = 'schema/'+req.body.filename+'.json';
-	fs.exists(filename,function(exists){
-		if(!exists) {
-			fs.appendFile(filename,JSON.stringify({'val':40}));
-			res.end("File " + req.body.filename+".json created.");
-		}
-		else {
-			res.end("File " +req.body.filename+".json already exists.");
-		}
-	});
+	var token = req.body.token;
+	var verified = verifyToken(token);
+	console.log(verified);
+	if(verified.error) {
+		res.send("Wrong token!");
+	}
+	else {
+		var filename = './schema/'+verified.name+'/'+req.body.filename+'.json';
+		fs.exists(filename,function(exists){
+			if(!exists) {
+				fs.appendFile(filename,JSON.stringify({'val':40}));
+				res.end("File " + req.body.filename+".json created.");
+			}
+			else {
+				res.end("File " +req.body.filename+".json already exists.");
+			}
+		});
+	}
 });
 
-app.get('/update/:file',function(req,res){
-	var filename = './schema/'+req.params.file+'.json';
-	console.log(filename);
-	fs.exists(filename,function(exists){
-		if(exists) {
-			var file = require(filename);
-			var url_parts = url.parse(req.url,'true');
-			var query = url_parts.query;
+app.get('/update/:token/:file',function(req,res){
+	var token = req.params.token;
+	var verified = verifyToken(token);
+	if(verified.error) {
+		res.send("Wrong token!");
+	}
+	else {
+		var filename = './schema/'+verified.name+'/'+req.params.file+'.json';
+		console.log(filename);
+		fs.exists(filename,function(exists){
+			if(exists) {
+				var file = require(filename);
+				var url_parts = url.parse(req.url,'true');
+				var query = url_parts.query;
 
-			//merging json objects, to be exported to another function 
-			var newFile={};
-			for(var key in file) newFile[key]=file[key];
-			for(var key in query) newFile[key]=query[key];
-			
-			fs.writeFile(filename, JSON.stringify(newFile), function (err) {
-			  if (err) return console.log(err)
-			  res.send('Updated file: ' + JSON.stringify(newFile));
-			});
-		}
-		else {
-			res.send("File " + res.file + " does not exist.")
-		}
-	});
+				//merging json objects, to be exported to another function 
+				var newFile={};
+				for(var key in file) newFile[key]=file[key];
+				for(var key in query) newFile[key]=query[key];
+				
+				fs.writeFile(filename, JSON.stringify(newFile), function (err) {
+				  if (err) return console.log(err)
+				  res.send('Updated file: ' + JSON.stringify(newFile));
+				});
+			}
+			else {
+				res.send("File " + res.file + " does not exist.")
+			}
+		});
+	}
 });
 
 app.post('/delete',function(req,res){
-	var filename = './schema/'+ req.body.filename + '.json';
-	fs.exists(filename,function(exists){
-		if(exists) {
-			fs.unlink(filename,function(err){
-				if(err)
-					res.send("Something went wrong!");
-				res.send('File '+ req.body.filename + ' deleted successfully');
-			})
-		}
-		else {
-			res.send('File ' + filename + ' does not exist.');
-		}
-	});
+	var token = req.body.token;
+	var verified = verifyToken(token);
+	if(verified.error) {
+		res.send("Wrong token!");
+	}
+	else {
+		var filename = './schema/'+verified.name+'/'+req.body.filename+'.json';
+		fs.exists(filename,function(exists){
+			if(exists) {
+				fs.unlink(filename,function(err){
+					if(err)
+						res.send("Something went wrong!");
+					res.send('File '+ req.body.filename + ' deleted successfully');
+				})
+			}
+			else {
+				res.send('File ' + filename + ' does not exist.');
+			}
+		});
+	}
 });
 
 app.post('/secret',function(req,res){
@@ -141,3 +170,17 @@ app.use(function(req,res,next){
 app.listen(app.get('port'),function(){
 	console.log("Listening on " + app.get('port'));
 });
+
+
+function verifyToken(token) {
+	var verified = {error:false};
+	try {
+		var decoded = jwt.verify(token,'microdb');
+		verified.name = decoded.name;
+		return verified;
+	}
+	catch(err) {
+		verified.error = true;
+		return verified;
+	}
+}
